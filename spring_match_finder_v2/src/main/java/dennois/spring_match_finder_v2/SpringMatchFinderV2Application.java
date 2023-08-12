df865950a58b42ad066c510e08e8a569f3f0030b
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-08-12 10:57:19 +0200

    First commit. Project setup and connected to DB

---
+++ spring_match_finder_v2/src/main/java/dennois/spring_match_finder_v2/SpringMatchFinderV2Application.java
@@ -0,0 +1,13 @@
+package dennois.spring_match_finder_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMatchFinderV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMatchFinderV2Application.class, args);
+    }
+
+}
---
+++ spring_match_finder_v2/src/test/java/dennois/spring_match_finder_v2/SpringMatchFinderV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dennois.spring_match_finder_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMatchFinderV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
